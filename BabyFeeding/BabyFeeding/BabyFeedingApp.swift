e01c30e91c843198e6c2e47f23d99520a6899294
temamarvel <tema.denisoff@gmail.com>
2023-12-03 11:59:49 +0400

    add empty app template

---
+++ BabyFeeding/BabyFeeding/BabyFeedingApp.swift
@@ -0,0 +1,18 @@
+//
+//  BabyFeedingApp.swift
+//  BabyFeeding
+//
+//  Created by Art Denisov on 03.12.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BabyFeedingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabyFeeding/BabyFeeding/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  BabyFeeding
+//
+//  Created by Art Denisov on 03.12.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Button("Button"){}
+    }
+}
+
+#Preview {
+    ContentView()
+}
