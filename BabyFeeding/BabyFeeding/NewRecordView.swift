7402140d3212e18bf5f3c433bb6c81b4cebf0c28
temamarvel <tema.denisoff@gmail.com>
2024-08-27 17:38:21 +0400

    use modelContext in NewRecordView

--- BabyFeeding/BabyFeeding/NewRecordView.swift
+++ BabyFeeding/BabyFeeding/NewRecordView.swift
@@ -9,20 +9,25 @@ import SwiftUI
 import SwiftData
 
 struct NewRecordView: View {
+    @Environment(\.modelContext) private var modelContext
     @Query private var records: [Record]
     @State private var text = ""
     
     var body: some View {
-        TextField("Enter product name", text: $text)
+        TextField("Enter product name", text: $text).onSubmit() {
+            let record = Record(productName: text, productCompany: "TestCompany", isGoodReaction: true)
+            modelContext.insert(record)
+        }
+        
         ScrollView(.horizontal) {
             LazyHStack(alignment: .top, spacing: 10) {
                 ForEach(1...100, id: \.self) { num in
                     Button(action: { text = "Qwert \(num)"}){
-                        Text("Test \(num)").font(.system(size: 9))
+                        Text("Test \(num)").font(.footnote)
                     }
-                        .buttonBorderShape(.capsule)
-                        .buttonStyle(.bordered)
-                        .tint(.red)
+                    .buttonBorderShape(.capsule)
+                    .buttonStyle(.bordered)
+                    .tint(.red)
                 }
             }.fixedSize()
         }.scrollIndicators(.hidden)
@@ -31,16 +36,16 @@ struct NewRecordView: View {
             LazyHStack(alignment: .top, spacing: 10) {
                 ForEach(records) { record in
                     Button(action: { text = "Qwert \(record.productName)"}){
-                        Text("Test \(record.productName)").font(.system(size: 9))
+                        Text("Test \(record.productName)").font(.system(size: 20))
                     }
                     .buttonBorderShape(.capsule)
                     .buttonStyle(.bordered)
-                    .tint(.red)
+                    .tint(.green)
                 }
             }.fixedSize()
         }.scrollIndicators(.hidden)
     }
-
+    
 }
 
 #Preview {
